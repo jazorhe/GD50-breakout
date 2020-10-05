70e0c1a8b2860ef438e0d98ec55bf550f5e765fe
jazorhe <jazor.he@gmail.com>
2020-10-05 22:07:55 +1100

    start state

---
+++ breakout-jazor/src/states/StartState.lua
@@ -0,0 +1,35 @@
+StartState = Class {__includes = BaseState}
+
+local hignlighted = 1
+
+function StartState:update(dt)
+
+    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('down') then hignlighted = hignlighted == 1 and 2 or 1
+        gSounds['paddle-hit']:play()
+    end
+
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+
+end
+
+function StartState:render()
+
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf("BREAKOUT", 0, VIRTUAL_HEIGHT / 3, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(gFonts['medium'])
+
+    if hignlighted == 1 then
+        love.graphics.setColor(0.4, 1, 1, 1)
+    end
+    love.graphics.printf("START", 0 , VIRTUAL_HEIGHT / 2 + 70, VIRTUAL_WIDTH, 'center')
+    love.graphics.setColor(255, 255, 255, 255)
+
+    if hignlighted == 2 then
+        love.graphics.setColor(0.4, 1, 1, 1)
+    end
+    love.graphics.printf("HIGH SCORES", 0, VIRTUAL_HEIGHT / 2 + 90, VIRTUAL_WIDTH, 'center')
+    love.graphics.setColor(255, 255, 255, 255)
+end
