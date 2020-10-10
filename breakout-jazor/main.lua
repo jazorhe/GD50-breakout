dda0aae3be94b36bad4609156770e98a4f5a2901
jazorhe <jazor.he@gmail.com>
2020-10-10 17:00:38 +1100

    gameover state

--- breakout-jazor/main.lua
+++ breakout-jazor/main.lua
@@ -54,7 +54,9 @@ function love.load()
 
     gStateMachine = StateMachine {
         ['start'] = function() return StartState() end,
-        ['play'] = function() return PlayState() end
+        ['play'] = function() return PlayState() end,
+        ['serve'] = function() return ServeState() end,
+        ['game-over'] = function() return GameOverState() end
     }
     gStateMachine:change('start')
 
