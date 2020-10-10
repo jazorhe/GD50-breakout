75d4f2e70769dc84ed698b1ef2c18c32c674f731
jazorhe <jazor.he@gmail.com>
2020-10-10 16:43:06 +1100

    collision with bricks

--- breakout-jazor/src/states/PlayState.lua
+++ breakout-jazor/src/states/PlayState.lua
@@ -1,11 +1,26 @@
+--[[
+    GD50
+    Breakout Remake
+
+    -- PlayState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents the state of the game in which we are actively playing;
+    player should control the paddle, with the ball actively bouncing between
+    the bricks, walls, and the paddle. If the ball goes below the paddle, then
+    the player should lose one point of health and be taken either to the Game
+    Over screen if at 0 health or the Serve screen otherwise.
+]]
+
 PlayState = Class{__includes = BaseState}
 
 function PlayState:init()
     self.paddle = Paddle()
-    self.paused = false
 
     -- initialize ball with skin #1; different skins = different sprites
-    self.ball = Ball(math.random(7))
+    self.ball = Ball(1)
 
     -- give ball random starting velocity
     self.ball.dx = math.random(-200, 200)
@@ -15,6 +30,7 @@ function PlayState:init()
     self.ball.x = VIRTUAL_WIDTH / 2 - 4
     self.ball.y = VIRTUAL_HEIGHT - 42
 
+    -- use the "static" createMap function to generate a bricks table
     self.bricks = LevelMaker.createMap()
 end
 
@@ -37,8 +53,23 @@ function PlayState:update(dt)
     self.ball:update(dt)
 
     if self.ball:collides(self.paddle) then
-        -- reverse Y velocity if collision detected between paddle and ball
+        -- raise ball above paddle in case it goes below it, then reverse dy
+        self.ball.y = self.paddle.y - 8
         self.ball.dy = -self.ball.dy
+
+        --
+        -- tweak angle of bounce based on where it hits the paddle
+        --
+
+        -- if we hit the paddle on its left side while moving left...
+        if self.ball.x < self.paddle.x + (self.paddle.width / 2) and self.paddle.dx < 0 then
+            self.ball.dx = -50 + -(8 * (self.paddle.x + self.paddle.width / 2 - self.ball.x))
+
+            -- else if we hit the paddle on its right side while moving right...
+        elseif self.ball.x > self.paddle.x + (self.paddle.width / 2) and self.paddle.dx > 0 then
+            self.ball.dx = 50 + (8 * math.abs(self.paddle.x + self.paddle.width / 2 - self.ball.x))
+        end
+
         gSounds['paddle-hit']:play()
     end
 
@@ -50,6 +81,50 @@ function PlayState:update(dt)
 
             -- trigger the brick's hit function, which removes it from play
             brick:hit()
+
+            --
+            -- collision code for bricks
+            --
+            -- we check to see if the opposite side of our velocity is outside of the brick;
+            -- if it is, we trigger a collision on that side. else we're within the X + width of
+            -- the brick and should check to see if the top or bottom edge is outside of the brick,
+            -- colliding on the top or bottom accordingly
+            --
+
+            -- left edge; only check if we're moving right
+            if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
+
+                -- flip x velocity and reset position outside of brick
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x - self.ball.width
+
+            -- right edge; only check if we're moving left
+            elseif self.ball.x + 6 > brick.x + brick.width and self.ball.dx < 0 then
+
+                -- flip x velocity and reset position outside of brick
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x + brick.width
+
+            -- top edge if no X collisions, always check
+            elseif self.ball.y < brick.y then
+
+                -- flip y velocity and reset position outside of brick
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y - self.ball.height
+
+            -- bottom edge if no X collisions or top collision, last possibility
+            else
+
+                -- flip y velocity and reset position outside of brick
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y + brick.height
+            end
+
+            -- slightly scale the y velocity to speed up the game
+            self.ball.dy = self.ball.dy * 1.02
+
+            -- only allow colliding with one brick, for corners
+            break
         end
     end
 
