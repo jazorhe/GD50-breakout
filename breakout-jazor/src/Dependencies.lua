574866e1edc0215df9879e55713a09363a0eea74
jazorhe <jazor.he@gmail.com>
2020-10-10 15:49:59 +1100

    level maker

--- breakout-jazor/src/Dependencies.lua
+++ breakout-jazor/src/Dependencies.lua
@@ -3,8 +3,10 @@ Class = require 'lib/class'
 
 require 'src/constants'
 require 'src/Ball'
+require 'src/Brick'
 require 'src/Paddle'
 require 'src/StateMachine'
+require 'src/LevelMaker'
 require 'src/Util'
 
 require 'src/states/BaseState'
